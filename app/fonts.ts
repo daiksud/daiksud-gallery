686658b1ced6b9781b94a49f653ab7fd20b0f4fe
Daiki Sudo <daiksud@proton.me>
2024-03-03 09:44:33 +0000

    feat: change fonts to Noto family

---
+++ app/fonts.ts
@@ -0,0 +1,11 @@
+import { Noto_Sans, Noto_Sans_JP } from 'next/font/google';
+
+export const noto_sans = Noto_Sans({
+  variable: '--font-noto-sans',
+  display: 'swap',
+});
+
+export const noto_sans_jp = Noto_Sans_JP({
+  variable: '--font-noto-sans-jp',
+  display: 'swap',
+});
